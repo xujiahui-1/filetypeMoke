57ba87d4ff3b895080b86700d42651c7f8b5ccc6
XuJiahui3 <106581608+XuJiahui3@users.noreply.github.com>
2023-06-08 15:18:44 +0900

    add type and matchers dir

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # filetypeMoke
+
+
+## 各文件夹含义
+
+- fixtures:各种文件的例子
+- types:最基本的类型结构体
+  - `defaults.go`:就是一个未知类型
+  - `split.go`:切割MIME的方法，返回前半段和后半段，其中前半段就是MIME的TYPE，后半段就是SUBTYPE
+  - `mime.go`:就是MIME结构体的声明和创建方法
+  - `type.go`:就是type结构体的声明和创建方法
+  - `types.go`:TYPES集合的声明，并且编辑了GET和ADD方法提供添加和获取TYPE操作
+- matchers:处理器等文件
+  - isobmff:处理image用的iso文件类型相关代码
+  - `document.go`:文本文件doc等等的处理器代码
+  - `matchers.go` 处理器的初始化等等
+
+--------
+
+1. 最底层结构体`Type`,他包含文件扩展名属性和MIME属性，`MIME`结构体又包括TYPE,SUBTYPE和VALUE，其中TYPE+SUBTYPE=VALUE
+2. `TYPES`其实就是个并发安全的map，用来存储所有的TYPE类型，
+3. 当我们创建新的TYPE类型的时候，在NewType方法中，就直接调用ADD方法将其放入了TYPES集合中了
+
+------
+>  各种文件的MIME请参见 https://developer.mozilla.org/zh-CN/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
--- matchers/matchers.go
+++ matchers/matchers.go
@@ -1,1 +1,50 @@
 package matchers
+
+import "filetyoe/types"
+
+// 把newType方法复制过来
+var newType = types.NewType
+
+// Matcher 这是matcher的结构体、文件类型匹配起
+type Matcher func([]byte) bool //传入一个字节数组，返回布尔
+
+// Map  定义一个map接口，各种类型的文件创建他并存入
+type Map map[types.Type]Matcher //键是Type实例，值是处理器方法
+
+// TypeMatcher 处理器
+type TypeMatcher func([]byte) types.Type
+
+// 存储已注册的文件类型匹配器
+var Matchers = make(map[types.Type]TypeMatcher)
+var MatcherKeys []types.Type //只存Matchers键的数组
+
+// NewMatcher 创建并存储一个新的 type matcher function
+func NewMatcher(kind types.Type, fn Matcher) TypeMatcher { //传入Type和Matcher，返回TypeMatcher
+	matcher := func(buf []byte) types.Type {
+		if fn(buf) { //这里的意思是如果把Type传入到了Matcher中，返回的是true，说明匹配，就返回type
+			return kind
+		}
+		return types.Unknown //不匹配，返回Unknow
+	}
+	//存到Matchers map中
+	Matchers[kind] = matcher
+	//在存到只存Matchers键的数组里
+	MatcherKeys = append([]types.Type{kind}, MatcherKeys...)
+	return matcher
+}
+
+func register(matchers ...Map) {
+	MatcherKeys = MatcherKeys[:0]
+	for _, m := range matchers {
+		for kind, matcher := range m {
+			NewMatcher(kind, matcher)
+		}
+	}
+}
+
+// 初始化的时候，把所有的类型的map，全部通过register方法，遍历创建新的NewMatcher，存到Matchers map中
+func init() {
+	// Arguments order is intentional
+	// Archive files will be checked last due to prepend above in func NewMatcher
+	register(Document)
+}
